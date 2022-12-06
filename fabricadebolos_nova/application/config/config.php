9c1fe56ab2585f20e572d1db9eab747c3faea208
Matheus Lopes <matheusluiz200599@gmail.com>
2022-12-06 09:32:52 -0300

    changing url

--- fabricadebolos_nova/application/config/config.php
+++ fabricadebolos_nova/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/fabricadebolos_nova';
+$config['base_url'] = 'https://'.$_SERVER['HTTP_HOST'].'/fabrica_de_bolos';
 
 /*
 |--------------------------------------------------------------------------
